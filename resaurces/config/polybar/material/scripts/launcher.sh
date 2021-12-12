3c3d4a62bf163e2ad218aa6d919b14f07195d2e8
ElevEsma <58746605+ElevEsma@users.noreply.github.com>
2021-12-12 13:25:25 +0100

    Edited launchpad

--- resaurces/config/polybar/material/scripts/launcher.sh
+++ resaurces/config/polybar/material/scripts/launcher.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/material/scripts/rofi/launcher.rasi
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+## Editor  : ElevEsma
+## Github  : @ElevEsma
+
+# Available Styles
+# >> Created and tested on : rofi 1.6.0-1
+#
+# blurry	blurry_full		kde_simplemenu		kde_krunner		launchpad
+# gnome_do	slingshot		appdrawer			appdrawer_alt	appfolder
+# column	row				row_center			screen			row_dock		row_dropdown
+
+theme="launchpad"
+dir="$HOME/.config/rofi/launchers/misc"
+
+# comment these lines to disable random style
+#themes=($(ls -p --hide="launcher.sh" $dir))
+#theme="${themes[$(( $RANDOM % 16 ))]}"
+
+rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
